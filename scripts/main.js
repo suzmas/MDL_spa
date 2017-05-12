f8f713cd71266a07562679ccf6730d1da8aab800
Suzan Sucro <suzansucro@gmail.com>
2017-05-11 19:25:48 -0700

    Fix iOS contact button bug

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,17 @@
 
  // temporary hack to control for MDL tab-panel issue on page load
  // fix this bug!
-document.addEventListener("DOMContentLoaded", setTimeout(function () {
-  const hiddenElems = document.querySelectorAll(".hidden");
-  for (let i = 0; i < hiddenElems.length; i++) {
+document.addEventListener("DOMContentLoaded", showHidden);
+
+function showHidden() {
+  const hiddenElems = document.querySelectorAll(".hidden"); for (let i = 0; i < hiddenElems.length; i++) {
     hiddenElems[i].classList.remove("hidden");
   }
-}, 1000));
+}
 
 
 function goToContact() {
+  console.log("click emitted");
   const activeElements = document.querySelectorAll('.is-active');
   const contactElements =
   document.querySelectorAll('.contact');
