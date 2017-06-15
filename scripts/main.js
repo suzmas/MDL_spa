9fb853c34803a110c64be4124d82666884584d40
Suzan Sucro <suzansucro@gmail.com>
2017-06-14 21:19:39 -0700

    Reformat js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
-
 // control for MDL formatting issue on page load
 window.addEventListener("load", showHidden);
 
 function showHidden() {
-  const hiddenElems = document.querySelectorAll(".hidden"); for (let i = 0; i < hiddenElems.length; i++) {
+  const hiddenElems = document.querySelectorAll(".hidden");
+  for (let i = 0; i < hiddenElems.length; i++) {
     hiddenElems[i].classList.remove("hidden");
   }
 }
@@ -11,8 +11,7 @@ function showHidden() {
 // Force MDL to activate "contact" navigation tab on mail icon click
 function goToContact() {
   const activeElements = document.querySelectorAll('.is-active');
-  const contactElements =
-  document.querySelectorAll('.contact');
+  const contactElements = document.querySelectorAll('.contact');
   for (let i = 0; i < activeElements.length; i++) {
     activeElements[i].classList.remove("is-active");
     contactElements[i].classList.add("is-active");
